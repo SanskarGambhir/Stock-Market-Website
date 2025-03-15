69f5de8c4453dd7ae53e079f00118d386b03d0be
Pratik-Biyani <pratik.biyani09@gmail.com>
2025-03-15 13:04:27 +0530

    Razorpay and gemini integrated

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import Login from "./components/login";
 import Register from "./components/register";
 import Layout from "./layout";
 import ErrorPage from "./components/errorpage";
+import ChatBot from "./components/Chatbot";
+import Pay from "./components/Pay";
 
 function App() {
   const { data } = useContext(AppContext);
@@ -21,6 +23,8 @@ function App() {
         <Route index element={<h1>{data}</h1>} />
         <Route path="about" element={<Login />} />
         <Route path="*" element={<ErrorPage />} />
+        <Route path="/chat" element={<ChatBot />} />
+        <Route path="/pay" element={<Pay />} />
       </Route>
       <Route path="*" element={<ErrorPage />} />
     </Routes>
