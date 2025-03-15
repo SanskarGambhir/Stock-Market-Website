cfa50142f8613590142dbd6a9eef164f808d8a36
Pratik-Biyani <pratik.biyani09@gmail.com>
2025-03-15 18:12:50 +0530

    Currency Converter bhi daaldiya

--- src/App.jsx
+++ src/App.jsx
@@ -20,6 +20,7 @@ import { sampleStocks } from "./Data/Stocks";
 import Watchlist from "./components/Watchlist";
 import StockPage from "./components/StockPage";
 import SIPCalculator from "./components/SIPCalculator";
+import CurrencyConverter from "./components/CurrencyConverter";
 
 function App() {
   const { data } = useContext(AppContext);
@@ -58,6 +59,7 @@ function App() {
             />
             <Route path="/watchlist/:symbol" element={<StockPage />} />
             <Route path="/sip" element={<SIPCalculator />} />
+            <Route path="/currency" element={<CurrencyConverter />} />
           </Routes>
         </main>
         {!hideNavbarFooter && <Footer />}
