f8e16956fc70fbb60709459354254ec05dcbf75d
Nishant0121 <142099904+Nishant0121@users.noreply.github.com>
2025-03-16 08:37:42 +0530

    deploy

--- vite.config.js
+++ vite.config.js
@@ -1,33 +1,37 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
-import { VitePWA } from 'vite-plugin-pwa'
+import { VitePWA } from "vite-plugin-pwa";
 import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(),
+  plugins: [
+    react(),
+    tailwindcss(),
     VitePWA({
-      registerType: 'autoUpdate', // Automatically update the service worker
+      registerType: "autoUpdate",
       manifest: {
-        name: 'My PWA App',
-        short_name: 'PWA App',
-        description: 'My awesome Progressive Web App!',
-        theme_color: '#ffffff',
+        name: "My PWA App",
+        short_name: "PWA App",
+        description: "My awesome Progressive Web App!",
+        theme_color: "#ffffff",
         icons: [
           {
-            src: 'pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
           },
           {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
-        ]
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
       },
-      // Optionally, you can add workbox options here
+      workbox: {
+        maximumFileSizeToCacheInBytes: 4000000, // Increase limit to 4MB
+      },
     }),
   ],
   resolve: {
